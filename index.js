0e6ee9b4d9490075d8f31c9fe4d02bcd785d49ef
Badr Almadhi <badr.almadhi95@gmail.com>
2024-01-02 21:08:10 -0700

    added prompt and readme file situp

--- index.js
+++ index.js
@@ -1,4 +1,60 @@
-const inquire = require('inquirer');
-const fs = require('fs');
+// import inquirer it is node.js library that create command line interface
+const inquire = require("inquirer");
+// node.js file system
+const fs = require("fs");
+
+const generateReadMeFile = require('./generateFile');
+
+const promptUser = () => {
+  return inquire.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your name?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title for your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Add a description of your project:",
+    },
+    {
+      type: "input",
+      name: "installation",
+      message: "Add installation instructor to your project:",
+    },
+    {
+      type: "input",
+      name: "usage",
+      message: "Add the usage information of your project:",
+    },
+    {
+      type: "input",
+      name: "credits",
+      message:
+        "Add collaborators, ex. sources used or any kind of support helped with your project:"
+    },
+    {
+      type: "list",
+      name: 'license',
+      message: 'Select what license did you use for your project:',
+      choices: ['MIT License', 'Apache License 2.0', 'General Public License (GPL) v3.0', 'BSD 2-Clause "Simplified" License'],
+    },
+    {
+      type: "input",
+      name: "contribution",
+      message: "Add the contribution guideline of your project:",
+    },
+    {
+      type: "input",
+      name: "features",
+      message: "Add features of your project:",
+    },
+  ]);
+};
 
 
