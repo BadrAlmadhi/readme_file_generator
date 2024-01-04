55fa108c065efa6372faf8bb8d553437205afb45
Badr Almadhi <badr.almadhi95@gmail.com>
2024-01-04 00:22:02 -0700

    added README template, and updated questions

--- generateFile.js
+++ generateFile.js
@@ -1,68 +1,83 @@
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {
+  if (!license) {
+    return `Invalid license badge`;
+  } else {
+    return `[![License: ${license}](https://img.shields.io/badge/License-${license}-yellow.svg)](https://opensource.org/licenses/${renderLicenseLink(
+      license
+    )})`;
+  }
+};
 
-const indexFile = require('./index');
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/mit/";
+    case "GPL":
+      return "https://www.gnu.org/licenses/gpl-3.0";
+    case "CC--0":
+      return "https://creativecommons.org/publicdomain/zero/1.0/";
+    default:
+      // Handle cases where the license is not recognized
+      return "Invalid license";
+  }
+};
 
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  if (!license) {
+    return ``;
+  } else {
+    return `## Licenses
+    This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+  }
+};
 
-const generateFile = () => {
-    `# <Your-Project-Title>
+// TODO: Create a function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.title} 
 
-    ## Description
-    
-    Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-    
-    - What was your motivation?
-    - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-    - What problem does it solve?
-    - What did you learn?
-    
-    ## Table of Contents (Optional)
-    
-    If your README is long, add a table of contents to make it easy for users to find what they need.
-    
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Credits](#credits)
-    - [License](#license)
-    
-    ## Installation
-    
-    What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-    
-    ## Usage
-    
-    Provide instructions and examples for use. Include screenshots as needed.
-    
-    
-    ## Credits
-    
-    List your collaborators, if any, with links to their GitHub profiles.
-    
-    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-    
-    If you followed tutorials, include links to those here as well.
-    
-    ## License
-    
-    The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-    
-    ---
-    
-    🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-    
-    ## Badges
-    
-    ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-    
-    Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
-    
-    ## Features
-    
-    If your project has a lot of features, list them here.
-    
-    ## How to Contribute
-    
-    If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-    
-    ## Tests
-    
-    Go the extra mile and write tests for your application. Then provide examples on how to run them here.`
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+## License
+${renderLicenseSection(data.license)}
+${renderLicenseBadge(data.license)}
+
+## Badges
+${data.badges}
+
+## Features
+${data.features}
+
+## For contact info
+
+If you have any questions about the project please reach out:
+
+GitHub : https://github.com/${data.github}
+Email: ${data.email}
+
+`;
 }
+
+module.exports = generateMarkdown;
--- index.js
+++ index.js
@@ -1,60 +1,146 @@
-// import inquirer it is node.js library that create command line interface
-const inquire = require("inquirer");
-// node.js file system
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 const fs = require("fs");
 
-const generateReadMeFile = require('./generateFile');
+// TODO: Create an array of questions for user input
+const questions = [
+  {
+    // add in readme template
+    type: "input",
+    name: "name",
+    message: "Welcome to the README file generator! To start please provide your name:",
+    validate: (nameInput) => {
+      if (nameInput) {
+        return true;
+      } else {
+        console.log(
+          `Please enter your name! You must credit yourself for your work`
+        );
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub Username?",
+    validate: (githubInput) => {
+      if (githubInput) {
+        return true;
+      } else {
+        console.log(
+          `Please enter your GitHub username! it is essential to show your projects to other developers.`
+        );
+      }
+    },
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+    validate: (emailInput) => {
+      if (emailInput) {
+        return true;
+      } else {
+        console.log(
+          `Please enter your email! For other developers to reach out!`
+        );
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: (titleInput) => {
+      if (titleInput) {
+        return true;
+      } else {
+        console.log(
+          `Your README file must contain a title.`
+        );
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Enter your project description here:',
+    validate: (descriptionInput) => {
+      if (descriptionInput) {
+        return true;
+      } else {
+        console.log(
+          `Please add a description to your project.`
+        );
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'Enter an installation steps for your project here:',
+    validate: (installationInput) => {
+      if (installationInput) {
+        return true;
+      } else {
+        console.log(
+          `Please provide installation steps for users to install tools needed for your project`
+        );
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Enter a usage steps for your project here:',
+    validate: (usageInput) => {
+      if (usageInput) {
+        return true;
+      } else {
+        console.log(
+          `Please provide usage steps for project`
+        );
+      }
+    },
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'Please if there are any credits help you with you project add it here:',
+    validate: (creditsInput) => {
+      if (creditsInput) {
+        return true;
+      } else {
+        console.log(
+          `Please provide credits such as documentation or anything help you with your project`
+        );
+      }
+    },
+  },
+  {
+    type: 'list',
+    name: 'credits',
+    choices: ["MIT", "GPL", "CC--0"],
+    validate: (creditsInput) => {
+      if (creditsInput) {
+        return true;
+      } else {
+        console.log(
+          `Please provide credits such as documentation or anything help you with your project`
+        );
+      }
+    },
+  },
 
-const promptUser = () => {
-  return inquire.prompt([
-    {
-      type: "input",
-      name: "username",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "title",
-      message: "What is the title for your project?",
-    },
-    {
-      type: "input",
-      name: "description",
-      message: "Add a description of your project:",
-    },
-    {
-      type: "input",
-      name: "installation",
-      message: "Add installation instructor to your project:",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "Add the usage information of your project:",
-    },
-    {
-      type: "input",
-      name: "credits",
-      message:
-        "Add collaborators, ex. sources used or any kind of support helped with your project:"
-    },
-    {
-      type: "list",
-      name: 'license',
-      message: 'Select what license did you use for your project:',
-      choices: ['MIT License', 'Apache License 2.0', 'General Public License (GPL) v3.0', 'BSD 2-Clause "Simplified" License'],
-    },
-    {
-      type: "input",
-      name: "contribution",
-      message: "Add the contribution guideline of your project:",
-    },
-    {
-      type: "input",
-      name: "features",
-      message: "Add features of your project:",
-    },
-  ]);
-};
+];
 
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
 
+// TODO: Create a function to initialize app
+function init() {
+  return inquirer.prompt(questions);
+}
+
+// Function call to initialize app
+init();
